dd03c34ebdea84bf2e5bf729a67082211a91a0b5
mephix <john.prins@gmail.com>
2020-12-15 16:49:52 -0800

    update prospects folder

--- prospects/prospectEngine.js
+++ prospects/prospectEngine.js
@@ -2,7 +2,7 @@ const computeScore = require('../scores/computeScore.js')
 
 module.exports = prospectEngine
 
-function prospectEngine (people) {
+function prospectEngine (users) {
   const subScores = {
     // Show people of their gender preference.
     'gender': { transform: z => z, weight: 1, score: [] },
@@ -13,12 +13,16 @@ function prospectEngine (people) {
     // Don't show someone they've dated already.
     'dated': { transform: z => !z, weight: 1, score: [] },
     // Show people of their age preference.
-    'age': { transform: z => z, weight: 1, score: [] },
+    'age': { transform: z => z, weight: 1, score: [], params: { T: 0 } },
     // Don't show someone themself.
     'notself': { transform: z => z, weight: 1, score: [] },
     // For prospects, the further away, the better.
     // Less chance of them being in the round.
     'location': { transform: z => (1-z), weight: 1, score: [] },
   }
-  return computeScore(people, subScores)
+  // Key by id for `computeScore`.
+  const usersById = []
+  users.map(({ id, ...rest }) => usersById[id] = { id, ...rest })
+
+  return computeScore(usersById, subScores)
 }
--- prospects/runProspectEngine.js
+++ prospects/runProspectEngine.js
@@ -1,7 +1,9 @@
-const getUsers = require('./getUsers.js')
+const getUsers = require('../getUsers.js')
 const formatUserFields = require('../formatUserFields.js')
 const prospectEngine = require('./prospectEngine.js')
 const writeScoresToFile = require('../scores/writeScoresToFile.js')
+const subsetScores = require('../scores/subsetScores.js')
+const adaloApi = require('../adaloApi.js')
 
 module.exports = runProspectEngine
 
@@ -9,7 +11,7 @@ module.exports = runProspectEngine
  * Test and run here
  */
 const PROSPECT_GRAPH_FILE = './csvs/Prospects (all positive).csv'
-const USER_LOCAL_FILE = './csvs/Users (prospects).json'
+const USER_LOCAL_FILE = './csvs/Users 1644 (ids added).json' //'./csvs/Users (prospects).json'
 const refresh = false
 
 runProspectEngine()
@@ -18,40 +20,32 @@ async function runProspectEngine() {
   console.log(`Starting Prospect Engine...`)
   // Get Users. refresh=false is for testing and debugging
   // because downloading the collections from Adalo is slow.
-  let users = getUsers({ refresh, fileName: USER_LOCAL_FILE })
-
-  // `people` is `users` keyed by id.
-  const people = []
-  users.map(({ id, ...rest }) => people[id] = formatUserFields(rest))
+  let users = await getUsers({ refresh, fileName: USER_LOCAL_FILE })
+  let people = users.map(formatUserFields)
 
   // Rank prospects according to people's preferences.
-  let { score, subScores } = prospectEngine(people)
+  let { score, subScores, peopleById } = prospectEngine(people)
 
-  // // Deep copy `prospectsByPreference`.
-  // let prospects = deepCopy(prospectsByPreference)
+  // Keep only scores above a cutoff.
+  const CUTOFF = 0.05
+  score = subsetScores(score, { above: CUTOFF })
 
-  // // Remove people who someone has already liked, nexted or dated.
-  // let { prospects, relationship: dated } =
-  //   removeProspects({ prospects, people, relationship: 'dated' })
-  // let { prospects, relationship: liked } =
-  //   removeProspects({ prospects, people, relationship: 'liked' })
-  // let { prospects, relationship: nexted } =
-  //   removeProspects({ prospects, people, relationship: 'nexted' })
-
-  // Write prospects to file, along with prefs and subscores.
+  // Write prospects to file, along with prefs and subscores.  
   writeScoresToFile({
-    people,
+    people: peopleById,
     score,
     subScores,
     fileName: PROSPECT_GRAPH_FILE,
   })
 
   // Post prospects to Adalo.
-  /*
-  Object.keys(prospects).map(id => {
-    adaloApi.update('Users', id, { prospects: Object.keys(prospects[id])})
+  // Object.keys(score)
+  // !! FOR DEBUGGING TRY JUST ONE !!
+  const ids = [Object.keys(score)[0]] // Object.keys(score)
+  ids.map(id => {
+    const prospects = Object.keys(score[id]).map(Number)
+    adaloApi.update('Users', id, { prospects })
   })
-  */
 
-  return prospects
+  return score
 }
