9bf8272f5ebb3a380fe7b9f678c618b09e704c75
mephix <john.prins@gmail.com>
2020-12-25 17:29:59 -0800

    minor bug fix

--- users/setProfileDefaults.js
+++ users/setProfileDefaults.js
@@ -15,10 +15,17 @@ function setProfileDefaults(user) {
     user.profile['Gender Preference'] =  'FMX'
   // 50 seems like the least worst default for age.
   user.profile['Age'] = user.profile['Age'] || 50
-  user.profile['Age Preference Low'] = user.profile['Age Preference Low'] || math.max(18, (age/2)+7)
-  user.profile['Age Preference High'] = user.profile['Age Preference High'] || math.min(99, (age-5)*1.5)
+  user.profile['Age Preference Low'] = user.profile['Age Preference Low'] || math.max(18, (user.profile['Age']/2)+7)
+  user.profile['Age Preference High'] = user.profile['Age Preference High'] || math.min(99, (user.profile['Age']-5)*1.5)
   // Default location is set to San Francisco (94110).
   user.profile['Zipcode'] = Number(user.profile['Zipcode']) || 94110
   user.profile['Radius'] = Number(user.profile['Radius']) || 50
+
+  // !! These M2M fields get omitted if they are empty !!
+  user['Prospects'] = user['Prospects'] || []
+  user['Likes'] = user['Likes'] || []
+  user['Nexts'] = user['Nexts'] || []
+  user['Dated'] = user['Dated'] || []
+
   return user
 }
