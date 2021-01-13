7f0995c28c6bdce0c47a94b036f91432e5120fb0
mephix <john.prins@gmail.com>
2021-01-13 10:13:43 -0800

    Working runDateEngine.

--- dates/runDateEngine.js
+++ dates/runDateEngine.js
@@ -2,14 +2,16 @@
 SET THESE PARAMS
 */
 let ROUND_ID = 1
-let DAY = '2021-01-11'
-let HOUR = 21
-let SLOT = 2
+let DAY = '2021-01-12'
+let HOUR = 16
+let SLOT = 5
 let RERUN = false
-let CUTOFF = 0.01
-
-// `throwError` in getSomeUsers should be FALSE unless debugging.
-let throwError = true
+// `cutoff` above zero will make the dateEngine more picky.
+// Useful for later rounds.
+let CUTOFF = 0
+// `avoid503` can be set to true to avoid a 503 error.
+// In this case it will only use the local backup of Users.
+let avoid503 = false
 
 // `seqOrPar` should be "parallel" unless there is a dire 503 error when
 // posting dates to Adalo. In that case, change it to to "sequential".
@@ -66,13 +68,13 @@ async function runDateEngine() {
    * WRITE A POSTIT WHEN USING THESE DEBUGGING IDS
    */
   // let idsOfUsersHere = [702,727,750,765,760,897] // 
-  let idsOfUsersHere = [726, 735, 738, 742, 772, 798, 828, 848, 877, 915, 934, 969, 971, 1018, 1020, 1023, 1027, 1029, 1030, 1031]
-  // let idsOfUsersHere = round.Here || []
+  // let idsOfUsersHere = [726, 735, 738, 742, 772, 798, 828, 848, 877, 915, 934, 969, 971, 1018, 1020, 1023, 1027, 1029, 1030, 1031]
+  let idsOfUsersHere = round.Here || []
   if (idsOfUsersHere) console.log(`${idsOfUsersHere.length} people are Here.`)
 
   // Download Users Here (or load Users and filter to Here).
   let { usersHere, usersUpdated } =
-    await getSomeUsers(idsOfUsersHere, TODAYS_USERS_FILE, throwError) //, method='sequential')
+    await getSomeUsers(idsOfUsersHere, TODAYS_USERS_FILE, avoid503) //, method='sequential')
   usersHere = usersHere.map(setProfileDefaults)
 
   // If slot rerun, filter for people who are Free.
@@ -86,7 +88,7 @@ async function runDateEngine() {
 
   // Load today's dates in case Users were loaded locally instead of downloaded.
   // Make sure Users have recorded who they dated today.
-  // Updates Watrueit Start Time of users with a date.
+  // Updates Start Time of users who had a date.
   let todaysDates
   [ usersHere, todaysDates] = addTodaysDates(usersHere, TODAYS_DATES_FILE)
 
@@ -109,14 +111,13 @@ async function runDateEngine() {
     // Add videochat Rooms to Dates.
     console.log(`Adding Daily rooms to Dates...`)
     const params = { DAY, HOUR, SLOT, TIMEZONE_OFFSET, SLOT_LENGTH, SLOT_PREENTRY, SLOT_STARTS, SLOT_ENDS }
-    // await Promise.all(dates.map(date => addRoom(date, params)))
+    await Promise.all(dates.map(date => addRoom(date, params)))
 
-    // Save the Dates locally.true
-    // Used to use `writeDatesFile`.
+    // Save the Dates locally
     writeToCsv([...todaysDates, ...dates], TODAYS_DATES_FILE)
 
     // Post the Dates to Adalo.
-    // postDates(dates, seqOrPar, params)
+    postDates(dates, seqOrPar, params)
 
   } else {
     console.log(`No dates created. Exiting`)
