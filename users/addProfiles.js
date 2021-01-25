dee6caa57e9c055baa56cceaebbf7322a94e1f5b
mephix <john.prins@gmail.com>
2021-01-25 10:12:21 -0800

    Reduce no-associated-profile to a warning.

--- users/addProfiles.js
+++ users/addProfiles.js
@@ -3,9 +3,15 @@ module.exports = addProfiles
 
 function addProfiles({ users, profiles }) {
   return users.map(user => {
-    let profile = profiles.filter(p => p.id === user['Profile'][0])
-    if (profile.length === 0) throw new Error(`Profile for user ${user['Email']} not found.`)
-    else if (profile.length > 1) throw new Error(`More than one profile for user ${user['Email']} found.`)
+    let profile = profiles.filter(p => p.id === user['Profile']?.[0])
+    if (profile.length === 0) {
+      console.warn(`Profile for user ${user['Email']} not found.`)
+      profile = null
+    }
+    else if (profile.length > 1) {
+      console.warn(`More than one profile for user ${user['Email']} found.`)
+      profile = profile[0]
+    }
     else profile = profile[0]
     return { ...user, profile}
   })
