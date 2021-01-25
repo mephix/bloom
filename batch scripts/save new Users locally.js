5c39451c5a953e534753e4e6a65886137db9ff7d
mephix <john.prins@gmail.com>
2021-01-25 10:13:04 -0800

    Increase checks for wrong fields.

--- batch scripts/save new Users locally.js
+++ batch scripts/save new Users locally.js
@@ -6,21 +6,39 @@ const fs = require('fs')
  * Test and run here
  */
 
-let existingUsersFile = './csvs/Users 2021-01-12 old1.json'
-let newFile =           './csvs/Users 2021-01-12.json'
-// let csvOutputFile = './csvs/Users 2021-01-12.csv'
+let existingUsersFile = './csvs/Users 2021 clean slate.json'
+let newFile =           './csvs/Users 2021-01-23.json'
 runGetNewUsers()
 
 async function runGetNewUsers() {
   console.log(`Starting runGetNewUsers...`)
   let { users } = await getNewUsers({ existingUsersFile, newFile })
-  // Don't also write in csv format because fields like Prospects are comma
-  // separated.
-  // csv.writeToCsv(users, csvOutputFile)
-  let noGender = users.filter(u => u.profile['Gender']===null)
-  let noGenderPref = users.filter(u => u.profile['Gender Preference']===null)
+  if (users) {
+    console.log(`Finished getting new users. Now checking them...`)
+    // Checks
+    // !! add a check for profile being missing !!
+    let checks = [
+      { name: 'Gender', value: null },
+      { name: 'Gender Preference', value: null },
+      { name: 'Finished', value: true },
+      { name: 'Free', value: false },
+    ]
+    checks.forEach( ({ name, value }) => {
+      console.log(`Checking ${name}...`)
+      let uf = users.filter(u => u?.profile[name]===value)
+      if (uf.length===0) console.log(`OK`)
+      uf.map(u => console.log(`${u.Email} has ${name}=${value}`))
+    })
+  } else {
+    console.log(`No new users returned.`)
+  }
 }
-  
+
+// let csvOutputFile = './csvs/Users 2021-01-12.csv'
+// Don't also write in csv format because fields like Prospects are comma
+// separated.
+// csv.writeToCsv(users, csvOutputFile)
+
 // let maxUsers = 350
 // let backupFile =    './csvs/Users 2021-01-11.json'
 // let newBackupFile = './csvs/Users 2021-01-11.json'
