6c23c6a72cf25d42c9f19ca5fefc0b927d310bd3
Michael Dunkle <michael.dunkle@gmail.com>
2021-01-25 18:23:14 -0700

    Adds Firebase and connects to db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
+import { db } from './config/firebase';
+
 import './assets/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      ...
-    </div>
-  );
-}
+export default class App extends React.Component {
+  componentDidMount() {
+    db.collection("Dates").where("ready", "==", true)
+      .onSnapshot((querySnapshot) => {
+          var dates: any[] = [];
+          querySnapshot.forEach((doc) => {
+            dates.push(doc.data());
+          });
+          console.log("Dates that are ready ", dates);
+      });
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        ...
+      </div>
+    );
+  }
+}
