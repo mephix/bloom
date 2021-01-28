447ca214476d41aab6e4953460cecbf080a0ad72
mephix <john.prins@gmail.com>
2021-01-28 10:44:27 -0800

    Display dates legibly before approving them.

---
+++ dates/displayPretty.js
@@ -0,0 +1,16 @@
+
+
+module.exports = displayPretty
+
+function displayPretty(dates, users) {
+  dates.forEach(d => {
+    try {
+      let p1 = users.find(u => u.Email === d.email1).profile
+      let p2 = users.find(u => u.Email === d.email2).profile
+      let p1str = `${p1.Gender}, ${p1.Age}, ${p1.City}, ${p1.Zipcode}`
+      let p2str = `${p2.Gender}, ${p2.Age}, ${p2.City}, ${p2.Zipcode}`
+      console.log(`matchscore ${d.matchscore.toPrecision(2)}: ${d.email1} ${d.email2}`)
+      console.log(`${d.name1} (${p1str}) with ${d.name2} (${p2str})`)
+    } catch (e) { console.warn(e) }
+  })
+}
