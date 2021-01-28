a8c5bdbbb2af42e010cba8801b568e548f44622e
mephix <john.prins@gmail.com>
2021-01-28 10:45:14 -0800

    Infer Gender Preference if we have Gender.

--- users/setProfileDefaults.js
+++ users/setProfileDefaults.js
@@ -11,13 +11,15 @@ function setProfileDefaults(user) {
   user['Posivibes'] = user['Posivibes']===0 ? 0 : (user['Posivibes'] || 1)
   // Gender default is nonbinary.
   user.profile['Gender'] = user.profile['Gender'] || 'X'
-  // Users are pansexual by default.
-  if (
-    !user.profile['Gender Preference']
-    || user.profile['Gender Preference']==='X'
-    || user.profile['Gender Preference']==='All'
-  )
-    user.profile['Gender Preference'] =  'FMX'
+  // Users are heterosexual if their gender is specified, and otherwise pansexual.
+  if (!user.profile['Gender Preference']) {
+    if (user.profile['Gender']==='M') user.profile['Gender Preference'] =  'F'
+    if (user.profile['Gender']==='F') user.profile['Gender Preference'] =  'M'
+    if (user.profile['Gender']==='X') user.profile['Gender Preference'] =  'FMX'
+  }
+  if (user.profile['Gender Preference']==='X')   user.profile['Gender Preference'] =  'FMX'
+  if (user.profile['Gender Preference']==='All') user.profile['Gender Preference'] =  'FMX'
+    
   // 50 seems like the least worst default for age.
   user.profile['Age'] = user.profile['Age'] || 50
   user.profile['Age Preference Low'] = user.profile['Age Preference Low'] || math.max(18, (user.profile['Age']/2)+7)
