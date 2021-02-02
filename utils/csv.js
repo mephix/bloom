0f4c2bf34ad16b6878e02fe66f5819340690cf85
mephix <john.prins@gmail.com>
2021-02-01 18:12:22 -0800

    Allow any delimiter in writeToCsv

--- utils/csv.js
+++ utils/csv.js
@@ -19,10 +19,10 @@ function readCsv(fileName) {
   return arr
 }
 
-function writeToCsv(obj, fileName) {
+function writeToCsv(obj, fileName, delimiter = ',') {
   const fields = Object.keys(obj[0])
-  const header = fields.join(',')
-  const body = obj.map(o => fields.map(f => `${o[f]}`).join(','))
+  const header = fields.join(delimiter)
+  const body = obj.map(o => fields.map(f => `${o[f]}`).join(delimiter))
   const fileContent = [header, ...body].join('\n')
   fs.writeFile(fileName, fileContent, function (err) {if (err) return console.log(err)})
   console.log(`Object written to file ${fileName}`) 
