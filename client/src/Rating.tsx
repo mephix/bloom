0fe91e9a278540f66137054df7bf86e368c4026f
Michael Dunkle <michael.dunkle@gmail.com>
2021-02-17 18:07:06 -0700

    Adds icon buttons

--- client/src/Rating.tsx
+++ client/src/Rating.tsx
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import { ThumbUp, Favorite } from '@material-ui/icons';
 
 import './assets/css/App.css';
 
@@ -43,13 +44,13 @@ class Rating extends React.Component<Props, State> {
             <div className="feedback">
               <p>Could you see and hear each other okay?</p>
               <IconButton color="primary" onClick={() => this.props.rateDate('fun')}>
-                ...
+                <ThumbUp />
               </IconButton>
             </div>
             <div className="feedback">
               <p>Do you want to exchange numbers with ?</p>
               <IconButton color="primary" onClick={() => this.props.rateDate('heart')}>
-                ...
+                <Favorite />
               </IconButton>
             </div>
             <AppBar position="fixed" color="primary" className="bottom-bar">
