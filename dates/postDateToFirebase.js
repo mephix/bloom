97231179acad75f17f46c54c66333b2eff62930b
mephix <john.prins@gmail.com>
2021-04-14 08:07:20 -0700

    Switch from 2 dates to 1.

--- dates/postDateToFirebase.js
+++ dates/postDateToFirebase.js
@@ -13,9 +13,10 @@ function postDateToFirebase(date) {
    */
 
   // Create Firestore ids and refs.
-  const { idDate1, idDate2 } = dateIdsForFirebase(date)
+  // const { idDate1, idDate2 } = dateIdsForFirebase(date)
+  const { idDate1 } = dateIdsForFirebase(date)
   const refDate1 = firestoreApi.db.collection('Dates').doc(idDate1)
-  const refDate2 = firestoreApi.db.collection('Dates').doc(idDate2)
+  // const refDate2 = firestoreApi.db.collection('Dates').doc(idDate2)
   const refRoom =  firestoreApi.db.collection('Rooms').doc(date.dailyRoomName)
   // const refUser1 = firestoreApi.db.collection('Users').doc(date.id1)
   // const refUser2 = firestoreApi.db.collection('Users').doc(date.id2)
@@ -26,20 +27,23 @@ function postDateToFirebase(date) {
     'end':   new Date(date.endTime),
     'room': refRoom,
     'active': true,
+    // Start with accepted=true, and later on don't set it.
+    'accepted': false,
   }
   // Firebase uses emails, not Adalo IDs.
   const for1 = {
     'for':  date.email1,
     'with': date.email2,
-    'token': date.token1,
+    // !! change back after debugging !!
+    'token': date.token1, //{ for: date.token1, with: date.token2 },
   }
-  const for2 = {
-    'for':  date.email2,
-    'with': date.email1,
-    'token': date.token2,
-  }
+  // const for2 = {
+  //   'for':  date.email2,
+  //   'with': date.email1,
+  //   'token': date.token2,
+  // }
   const date1 = { ...commonFields, ...for1, }
-  const date2 = { ...commonFields, ...for2, }
+  // const date2 = { ...commonFields, ...for2, }
 
   // Create the Room.
   const room = {
@@ -47,13 +51,14 @@ function postDateToFirebase(date) {
     exp: date.exp,
     name: date.dailyRoomName,
     url: date.dailyRoomURL,
-    dates: [refDate1, refDate2],
+    date: refDate1,
+    // dates: [refDate1, refDate2],
   }
 
-  // Add the two Dates and their Room in a batch.
+  // Add the Date and their Room in a batch.
   const batch = firestoreApi.db.batch()
   batch.set(refDate1, date1)
-  batch.set(refDate2, date2)
+  // batch.set(refDate2, date2)
   batch.set(refRoom, room)
   return batch.commit()
 }
