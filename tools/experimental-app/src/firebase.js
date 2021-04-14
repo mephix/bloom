bd3051b0555def8b6df996886b10c545d1863eb5
rlee0 <ryanhlee0@gmail.com>
2021-04-14 12:53:33 -0700

    upd

--- tools/experimental-app/src/firebase.js
+++ tools/experimental-app/src/firebase.js
@@ -2,16 +2,17 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgNTDmX1h96iiRR8sveNvzLFHzCqrJrQg",
-  authDomain: "bloom-dating.firebaseapp.com",
-  databaseURL: "https://bloom-dating.firebaseio.com",
-  projectId: "bloom-dating",
-  storageBucket: "bloom-dating.appspot.com",
-  messagingSenderId: "671960082645",
-  appId: "1:671960082645:web:a7867703decb3b8bc19a5c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
 firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
 export const time = firebase.firestore.Timestamp;
+
+firebase.initializeApp(firebaseConfig);
