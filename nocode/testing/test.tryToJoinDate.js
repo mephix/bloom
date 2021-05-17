acf3bfd775980cffba5c82a721ff3936b04416d7
John <john.prins@gmail.com>
2021-05-17 12:13:56 -0700

    Add 5th test date to test file

--- nocode/testing/test.tryToJoinDate.js
+++ nocode/testing/test.tryToJoinDate.js
@@ -6,10 +6,11 @@ testTryToJoinDate()
 
 async function testTryToJoinDate() {
     const batch = db.batch()
+    batch.update(db.collection('Dates').doc('iHUZv6SWtRGWtSFLkIHK'), { active: true, accepted: null })
+    batch.update(db.collection('Dates').doc('gesAc2QQ5FX3R1Wd0miA'), { active: true, accepted: null })
     batch.update(db.collection('Dates').doc('fhRntWNybEQRgwFZ8thB'), { active: true, accepted: null })
     batch.update(db.collection('Dates').doc('SnluiFmrpnwG8rvOEuG2'), { active: true, accepted: null })
-    batch.update(db.collection('Dates').doc('gesAc2QQ5FX3R1Wd0miA'), { active: true, accepted: null })
-    batch.update(db.collection('Dates').doc('iHUZv6SWtRGWtSFLkIHK'), { active: true, accepted: null })
+    batch.update(db.collection('Dates').doc('QY3343yj4JvdKkhzBXnx'), { active: true, accepted: null })
     await batch.commit()
     await tryToJoinDate('fhRntWNybEQRgwFZ8thB')
     console.log('')
