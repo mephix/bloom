dd86324c53dbacb60b8de91403493a56ac07d6a6
John <john.prins@gmail.com>
2021-05-17 10:43:35 -0700

    fix tryToJoinDate

--- nocode/matchmaker/tryToJoinDate.js
+++ nocode/matchmaker/tryToJoinDate.js
@@ -1,68 +1,91 @@
-import { db, time } from '../firebase'
+const firestore = require('../apis/firestoreApi.js')
+const db = firestore.db
+const time = firestore.db.Timestamp
 
-export default function tryToJoinDate(dateId) {
-    firebase.modify('Dates', dateToJoin.id, {
-        accepted: true,
-        timeReplied: firebase.db.Timestamp.now(),
-    })
+module.exports = tryToJoinDate
+
+async function tryToJoinDate(dateId) {
     const dateRef = db.collection('Dates').doc(dateId)
-    const success = await runTransaction(db, async (transaction) => {
+    const success = await db.runTransaction(async (transaction) => {
         const date = await transaction.get(dateRef)
 
         // Only accept the date if it is still active.
-        // Otherwise, do nothing.
+        // Otherwise, return.
         if (date.data().active) {
             const sender = date.data().with
             const recipient = date.data().for
 
-            // Get all other current active dates for the recipient
-            const datesForRecipient = 
+            // Query all current active dates for and with the sender and recipient.
+            let datesForSenderQ = 
                 await transaction.get(
-                    db.collection(DATES_COLLECTION)
-                        .where('for', '==', recipient)
-                        .where('active', '==', true)
-                        .where('end', '>', time.now())
-                )
-            // Get all other current active dates with the recipient
-            const datesWithRecipient = 
-                await transaction.get(
-                    db.collection(DATES_COLLECTION)
-                        .where('with', '==', recipient)
-                        .where('active', '==', true)
-                        .where('end', '>', time.now())
-                )
-            // Get all other current active dates for the sender
-            const datesForSender = 
-                await transaction.get(
-                    db.collection(DATES_COLLECTION)
+                    db.collection('Dates')
                         .where('for', '==', sender)
                         .where('active', '==', true)
                         .where('end', '>', time.now())
                 )
-      
-            // Set accepted=false for all other current active dates for the recipient
-            datesForRecipient.map(d => {
+
+            let datesForRecipientQ = 
+                await transaction.get(
+                    db.collection('Dates')
+                        .where('for', '==', recipient)
+                        .where('active', '==', true)
+                        .where('end', '>', time.now())
+                )
+
+            let datesWithSenderQ = 
+                await transaction.get(
+                    db.collection('Dates')
+                        .where('with', '==', sender)
+                        .where('active', '==', true)
+                        .where('end', '>', time.now())
+                )
+
+            let datesWithRecipientQ = 
+                await transaction.get(
+                    db.collection('Dates')
+                        .where('with', '==', recipient)
+                        .where('active', '==', true)
+                        .where('end', '>', time.now())
+                )
+
+            // Get the docs from the queries, excluding the date we are accepting.
+            let datesForSender = datesForSenderQ.docs.filter(d => d.id !== date.id)
+            let datesForRecipient = datesForRecipientQ.docs.filter(d => d.id !== date.id)
+            let datesWithSender = datesWithSenderQ.docs.filter(d => d.id !== date.id)
+            let datesWithRecipient = datesWithRecipientQ.docs.filter(d => d.id !== date.id)
+
+            // Logging output: how many dates of each kind.
+            console.log(`datesForSender: ${datesForSender.length}`)
+            console.log(`datesForRecipient: ${datesForRecipient.length}`)
+            console.log(`datesWithSender: ${datesWithSender.length}`)
+            console.log(`datesWithRecipient: ${datesWithRecipient.length}`)
+    
+            // Decline (accepted=false) all the other current active dates for the sender and recipient.
+            console.log('Declining other dates...')
+            let datesFor = [...datesForSender, ...datesForRecipient]
+            datesFor.map(d => {
                 transaction.update(d.ref, { accepted: false })
             })
 
-            // Set active=false for all other current active dates with the recipient
-            datesWithRecipient.map(d => {
-                transaction.update(d.ref, { active: false })
-            })
-
-            // Set active=false for all other current active dates for the sender
-            datesForSender.map(d => {
-                transaction.update(d.ref, { active: false })
-            })
+            // Deactivate (active=false) all the other current active dates with the sender and recipient.
+            console.log('Deactivating other dates...')
+            let datesWith = [...datesWithSender, ...datesWithRecipient]
+            datesWith.map(d => {
+                    transaction.update(d.ref, { active: false })
+                })
 
             // Accept the date.
+            console.log('Accepting the successful date...')
             transaction.update(dateRef, {
+                active: true,
                 accepted: true,
                 timeReplied: time.now(),
             })
+            return true
+        } else {
+            // If the date is no longer active, return without taking any action.
+            return false
         }
     })
     return success
 }
-
-
---
+++ nocode/testing/test.tryToJoinDate.js
@@ -0,0 +1,16 @@
+const tryToJoinDate = require('../matchmaker/tryToJoinDate.js')
+const firestore = require('../apis/firestoreApi.js')
+const db = firestore.db
+
+testTryToJoinDate()
+
+async function testTryToJoinDate() {
+    const batch = db.batch()
+    batch.update(db.collection('Dates').doc('fhRntWNybEQRgwFZ8thB'), { active: true, accepted: null })
+    batch.update(db.collection('Dates').doc('SnluiFmrpnwG8rvOEuG2'), { active: true, accepted: null })
+    batch.update(db.collection('Dates').doc('gesAc2QQ5FX3R1Wd0miA'), { active: true, accepted: null })
+    batch.update(db.collection('Dates').doc('iHUZv6SWtRGWtSFLkIHK'), { active: true, accepted: null })
+    await batch.commit()
+    await tryToJoinDate('fhRntWNybEQRgwFZ8thB')
+    console.log('')
+}
