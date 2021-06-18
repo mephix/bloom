081c96be11fecf26ffcbcf7ec077dededee20f95
Nikita Fishbakh <n.fishbakh.dev@gmail.com>
2021-06-18 11:12:38 +0300

    fix firebase cache and add ios firebase conf

--- mobile/ios/App/App/AppDelegate.swift
+++ mobile/ios/App/App/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-         FirebaseApp.configure()
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
