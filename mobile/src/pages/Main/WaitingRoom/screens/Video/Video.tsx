6f2e9312d6140e556e4ce47f7befdbb50df95f04
Nikita Fishbakh <n.fishbakh.dev@gmail.com>
2021-06-21 11:40:09 +0300

    add fade out animation on date ending

--- mobile/src/pages/Main/WaitingRoom/screens/Video/Video.tsx
+++ mobile/src/pages/Main/WaitingRoom/screens/Video/Video.tsx
@@ -1,5 +1,12 @@
 import DailyIframe, { DailyCall } from '@daily-co/daily-js'
-import { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react'
 import Countdown from 'react-countdown'
 import moduleStyles from './Video.module.scss'
 import app from 'state/app'
@@ -7,7 +14,7 @@ import meetup from 'state/meetup'
 import { fromSecondsToMillis } from 'services/dateClock.service'
 import { CountDown } from './CountDown'
 import user from 'state/user'
-import { Logger } from 'utils'
+import { classes, Logger } from 'utils'
 import { TabContext } from 'routes'
 import commonStyles from '../Common.module.scss'
 
@@ -15,16 +22,22 @@ const logger = new Logger('Video', 'red')
 
 const COUNT_DOWN = 10
 
+const fadeInAnimation = {
+  animation: 'linear 10s inAnimation'
+}
+
 export const Video = () => {
   const defaultEndDateTime = Date.now() + 10 * 60 * 60 * 1000
   const videoFrameRef = useRef<HTMLIFrameElement>(null)
   const [dailyObj, setDailyObj] = useState<DailyCall | null>(null)
   const [endDateTime, setEndDateTime] = useState(defaultEndDateTime)
   const [visibleCountDown, setVisibleCountDown] = useState(true)
-  const [timeout] = useState(Date.now() + COUNT_DOWN * 1000)
+  const [dateEnding, setDateEnding] = useState(false)
+  const timeout = useMemo(() => Date.now() + COUNT_DOWN * 1000, [])
+  // const [timeout] = useState(Date.now() + COUNT_DOWN * 1000)
+
   const { hideTabs } = useContext(TabContext)
   const endDate = useCallback(() => {
-    // alert('end date')
     if (!dailyObj) return app.setRaitingState()
     dailyObj.leave()
   }, [dailyObj])
@@ -49,7 +62,11 @@ export const Video = () => {
       logger.error('No date was available for video chat.')
       app.setWaitingRoomState()
     }
-    setEndDateTime(fromSecondsToMillis(meetup.getEndTime()))
+    const dateEndTime = fromSecondsToMillis(meetup.getEndTime())
+    setEndDateTime(dateEndTime)
+    setTimeout(() => {
+      console.log('end date 10 sec')
+    }, dateEndTime - 10 * 1000)
     const roomUrl = `${room?.url}?t=${room?.token}`
     if (!videoFrameRef.current) return
     const daily = DailyIframe.wrap(videoFrameRef.current, {
@@ -58,11 +75,9 @@ export const Video = () => {
       showParticipantsBar: false
     })
     daily.on('joined-meeting', () => {
-      // setConnecting(false)
       daily.setLocalAudio(false)
     })
     daily.on('left-meeting', () => {
-      // alert('left why?')
       app.setRaitingState()
       meetup.setLeftTime()
     })
@@ -85,6 +100,12 @@ export const Video = () => {
           timeout={timeout}
         />
       )}
+      {dateEnding && (
+        <div
+          className={classes(commonStyles.container, moduleStyles.fade)}
+          style={fadeInAnimation}
+        />
+      )}
       <header className={moduleStyles.header}>
         <div className={moduleStyles.firstName}>
           {meetup.currentMatchingUserData?.firstName}
@@ -93,12 +114,16 @@ export const Video = () => {
           date={endDateTime}
           onComplete={endDate}
           renderer={({ total, seconds }) => {
-            const totalMin = total / 60 / 1000
+            const totalMin = Math.floor(total / 60 / 1000)
+            console.log(totalMin, seconds)
+            if (totalMin <= 0 && seconds <= 10 && !dateEnding) {
+              setDateEnding(true)
+            }
             return (
               <div
                 className={totalMin < 1 ? 'video-timer ending' : 'video-timer'}
               >
-                <div className="video-timer-panel">{Math.floor(totalMin)}</div>
+                <div className="video-timer-panel">{totalMin}</div>
                 <div className="video-timer-panel">{seconds}</div>
               </div>
             )
